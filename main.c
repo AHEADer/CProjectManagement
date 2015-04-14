16924cb86952463cd4bcab6ca759cafc5afaa9b3
Zhang Junda <jundazhang@hustunique.com>
2015-04-14 23:29:25 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include "functions.h"
+int main(int argc, char *argv[]){
+    gtk_init(&argc, &argv);
+    build_UI();
+    file_new();
+    gtk_main();
+    return 0;
+}
